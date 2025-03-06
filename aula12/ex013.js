6439c390b6d3e4fa558dc6412f4284f68e24d328
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-06 09:34:57 -0300

    criei o exercício 13 da aula 12

---
+++ aula12/ex013.js
@@ -0,0 +1,38 @@
+var agr = new Date()
+var semana = agr.getDay() //Dias da semana 
+
+//console.log(semana)
+
+switch (semana) {
+  case 0:
+    console.log('Domingo')
+    break
+
+    case 1:
+    console.log('Segunda')
+    break
+
+    case 2:
+    console.log('Terça')
+    break
+
+    case 3:
+    console.log('Quarta')
+    break
+
+    case 4:
+    console.log('Quinta')
+    break
+
+    case 5:
+    console.log('Sexta')
+    break
+
+    case 6:
+    console.log('Sábado')
+    break
+
+    default: 
+    console.log('Dia inválido')
+    break
+}
