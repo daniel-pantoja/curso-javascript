b1ee874eb77ca42a83e5c72f22d74ad64b8e13dd
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-23 19:58:31 -0300

    adicionar Vetor em JS

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -10,3 +10,29 @@ if (pos == -1) {
 } else {
   console.log(`O valor 8 está na posição ${pos}`)
 }
+
+let numeros = [5, 3, 8, 1, 4];
+numeros.push(7); 
+numeros.sort((a, b) => a - b); 
+console.log(numeros);
+console.log(`O vetor de números tem ${numeros.length} posições`);
+console.log(`O primeiro número do vetor é ${numeros[0]}`);
+let posNumero = numeros.indexOf(3);
+if (posNumero == -1) {
+  console.log('O número não foi encontrado!');
+} else {
+  console.log(`O número 3 está na posição ${posNumero}`);
+}
+
+let frutas = ['Banana', 'Morango', 'Laranja', 'Pera'];
+frutas.push('Uva'); 1
+frutas.sort(); 
+console.log(frutas);
+console.log(`O vetor de frutas tem ${frutas.length} posições`);
+console.log(`A primeira fruta do vetor é ${frutas[0]}`);
+let posFruta = frutas.indexOf('Laranja');
+if (posFruta == -1) {
+  console.log('A fruta não foi encontrada!');
+} else {
+  console.log(`A fruta Laranja está na posição ${posFruta}`);
+}
