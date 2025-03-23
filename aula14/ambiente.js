9c405890bc74405697b37019dfd45200d435c115
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-23 19:34:23 -0300

    adicionar contagem com FOR em JS

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -4,11 +4,37 @@ while (repetir <= 5) {
   console.log(repetir)
 }
 */
-
-console.log('Vamos contar')
+// Contando normal
+console.log("Vamos contar");
 
 for (var repetir = 1; repetir <= 5; repetir++) {
-  console.log(repetir)
+  console.log(repetir);
 }
 
-console.log('Fim da contagem.')
+console.log("Fim da contagem.");
+
+console.log("Contagem de 5 a 15:");
+
+for (var numero = 5; numero <= 15; numero++) {
+  console.log(numero);
+}
+
+console.log("Fim da contagem.");
+
+// Contagem regressiva
+console.log("Contagem regressiva de 10 a 1:");
+
+for (var reg = 10; reg >= 1; reg--) {
+  console.log(reg);
+}
+
+console.log("Fim da contagem.");
+
+// Números pares
+console.log("Números pares de 0 a 10:");
+
+for (var par = 0; par <= 10; par += 2) {
+  console.log(par);
+}
+
+console.log("Fim da contagem.");
---
+++ aula14/node_e597100821e3b.tmp.js
@@ -0,0 +1,22 @@
+
+var repetir = 1
+while (repetir <= 5) {
+  console.log(repetir)
+}
+
+
+console.log('Vamos contar') // 1
+
+for (var repetir = 1; repetir <= 5; repetir++) {
+  console.log(repetir)
+}
+
+console.log('Fim da contagem.')
+
+console.log('Contagem de 5 a 15:'); // 2
+
+for (var numero = 5; numero <= 15; numero++) {
+  console.log(numero);
+}
+
+console.log('Fim da contagem.');
