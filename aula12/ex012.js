d403ca32bf94b18ae358f5b10df52b3590514f89
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-23 18:59:12 -0300

    agrega exercício 12 de JavaScript

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,10 +1,32 @@
-var agora = new Date()
-var hora = agora.getHours()
-console.log(`Agora são exatamente ${hora} horas.`)
+var agora = new Date();
+var hora = agora.getHours();
+console.log(`Agora são exatamente ${hora} horas.`);
+
 if (hora < 12) {
-  console.log('Bom Dia!')
+  console.log("Bom Dia!");
 } else if (hora <= 18) {
-  console.log('Boa Tarde!')
+  console.log("Boa Tarde!");
 } else {
-  console.log('Boa Noite!')
+  console.log("Boa Noite!");
+}
+
+var dataAtual = new Date();
+var dia = dataAtual.getDay();
+console.log(`Hoje é dia ${dia}.`);
+
+if (dia === 0 || dia === 6) {
+  console.log("Final de semana!");
+} else {
+  console.log("Dia de semana!");
+}
+
+var temperaturaAtual = 30;
+console.log(`A temperatura atual é ${temperaturaAtual}°C.`);
+
+if (temperaturaAtual < 15) {
+  console.log('Está frio.');
+} else if (temperaturaAtual <= 25) {
+  console.log('Está agradável.');
+} else {
+  console.log('Está quente.');
 }
