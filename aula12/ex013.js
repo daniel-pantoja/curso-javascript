a51966df6c344200c6c7085b2aae19b12a6dd073
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-23 19:02:50 -0300

    agrega exercício 13 de JavaScript

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -1,38 +1,62 @@
-var agr = new Date()
-var semana = agr.getDay() //Dias da semana 
+var agr = new Date();
+var semana = agr.getDay(); //Dias da semana
 
 //console.log(semana)
 
 switch (semana) {
   case 0:
-    console.log('Domingo')
-    break
+    console.log("Domingo");
+    break;
 
-    case 1:
-    console.log('Segunda')
-    break
+  case 1:
+    console.log("Segunda");
+    break;
 
-    case 2:
-    console.log('Terça')
-    break
+  case 2:
+    console.log("Terça");
+    break;
 
-    case 3:
-    console.log('Quarta')
-    break
+  case 3:
+    console.log("Quarta");
+    break;
 
-    case 4:
-    console.log('Quinta')
-    break
+  case 4:
+    console.log("Quinta");
+    break;
 
-    case 5:
-    console.log('Sexta')
-    break
+  case 5:
+    console.log("Sexta");
+    break;
 
-    case 6:
-    console.log('Sábado')
-    break
+  case 6:
+    console.log("Sábado");
+    break;
 
-    default: 
-    console.log('Dia inválido')
-    break
+  default:
+    console.log("Dia inválido");
+    break;
+}
+
+var categoria = "F";
+
+switch (categoria) {
+  case "A":
+    console.log("Categoria A");
+    break;
+
+  case "B":
+    console.log("Categoria B");
+    break;
+
+  case "C":
+    console.log("Categoria C");
+    break;
+
+  case "D":
+    console.log("Categoria D");
+    break;
+
+  default:
+    console.log("Categoria inválida");
+    break;
 }
