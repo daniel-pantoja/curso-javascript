5d64b7b2a017f9f094f819675e9c5f5d96b94a7f
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-23 18:17:06 -0300

    agrega exercício 08 de JavaScript

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,4 +1,5 @@
-var velo = 50.7
+// Ex 1
+var velo = 50.7 
 
 console.log(`Sua velocidade é ${velo}km/h`)
 
@@ -6,3 +7,42 @@ if (velo > 40) {
   console.log(`Você ultrapassou a velocidade de 40km/h, você foi MULTADO!!!`)
 }
 
+if (velo > 60) {
+  console.log(`Cuidado! Você está acima do limite de 60km/h, sua velocidade é ${velo}km/h.`);
+} 
+
+// Ex 2
+velo = 22.2;
+
+console.log(`Sua velocidade é ${velo}km/h`);
+
+if (velo > 40) {
+  console.log(`Você ultrapassou a velocidade de 40km/h, você foi MULTADO!!!`);
+} else {
+  console.log(`Você está dentro do limite de velocidade, continue assim!`);
+}
+
+// Ex 3
+velo = 100;
+
+console.log(`Sua velocidade é ${velo}km/h`);
+
+if (velo > 40) {
+  console.log(`Você ultrapassou a velocidade de 40km/h, você foi MULTADO!!!`);
+}
+if (velo > 60) {
+  console.log(`Cuidado! Você está acima do limite de 60km/h, sua velocidade é ${velo}km/h.`);
+}
+
+// Ex 4
+velo = 4.1;
+
+console.log(`Sua velocidade é ${velo}km/h`);
+
+if (velo > 40) {
+  console.log(`Você ultrapassou a velocidade de 40km/h, você foi MULTADO!!!`);
+} else if (velo <= 40 && velo >= 30) {
+  console.log(`Sua velocidade é aceitável, mas poderia ser melhor.`);
+} else {
+  console.log(`Você está bem abaixo do limite de velocidade, mantenha-se alerta!`);
+}
