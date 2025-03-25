cc39fa96582ca3be30bd93df4244d2d9e17ef4bc
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-25 19:43:25 -0300

    adicionar Console

--- aula16/funcao01.js
+++ aula16/funcao01.js
@@ -1,9 +1,23 @@
 function parimpar(n) {
-  if (n%2 == 0) {
-    return 'Par!'
+  if (n % 2 == 0) {
+    return "Par!";
   } else {
-    return 'Impar!'
+    return "Impar!";
   }
 }
 
-console.log(parimpar(276621))
+console.log(parimpar(276621));
+console.log(parimpar(42));
+console.log(parimpar(99));
+console.log(parimpar(1001));
+console.log(parimpar(2048));
+console.log(parimpar(13));
+console.log(parimpar(-10));
+console.log(parimpar(-15));
+console.log(parimpar(1029));
+console.log(parimpar(0));
+console.log(parimpar(777));
+console.log(parimpar(-2));
+console.log(parimpar(-11));
+console.log(parimpar(54));
+console.log(parimpar(1));
