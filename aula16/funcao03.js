321a539dae4b35db2f36c4475f12647ebcc630ae
Daniel Pantoja <danieljuniorpantojapureza@gmail.com>
2025-03-27 16:58:40 -0300

    adicionar função 3 em JavaScript

--- aula16/funcao03.js
+++ aula16/funcao03.js
@@ -1,4 +1,20 @@
-let v = function(x) {
-  return x*2
-}
-console.log(v(19))
+let v = function (x) {
+  return x * 2;
+};
+console.log(v(8));
+
+let quadrado = function (y) {
+  return y * y;
+};
+console.log(quadrado(4));
+
+let ehPar = function (num) {
+  return num % 2 === 0;
+};
+console.log(ehPar(8));
+console.log(ehPar(7));
+
+let maior = function (x, y) {
+  return x > y ? x : y;
+};
+console.log(maior(12, 25));
